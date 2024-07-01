07feed2c032b48e2f1fe52240c4aaa5e1fdc1e0b
sahan <101819553+omerfshan@users.noreply.github.com>
2024-07-01 09:32:09 +0300

    1

---
+++ example.py
@@ -0,0 +1,2 @@
+print("hello")
+print("{} sayısının {} ile çarpımının sonucu: {}".format(12,10,12*10))
---
+++ numbers_1.py
@@ -0,0 +1,12 @@
+print(type(2))
+# type hangi veri tipi olduğunu yazdırır
+print(2+2)
+print(type(2))     # <class 'int'>
+print(type(2.5))   # <class 'float'>
+print(type(5.0))   # <class 'float'>
+print(type(5))     # <class 'int'>
+print(type(0.0))   # <class 'float'>
+print(type(0))     # <class 'int'>
+print(2+2.0)       # 4.0
+print(type(2+2.0)) # <class 'float'>
+print(type(2+2))   # <class 'int'>
