d4c8fa483024be5976d79840e04fc0f64b80fe57
sahan <101819553+omerfshan@users.noreply.github.com>
2024-07-01 12:09:38 +0300

    add

---
+++ data-typrs-demo.py
@@ -0,0 +1,9 @@
+pi=3.14
+r=float(input("r:"))
+
+print("dairenin cevresi {} dairenin alanı:{}".format(round(2*pi*r),round(pi*r**2)))
+
+km=float(input("km:"))
+
+print("mil:",round( km/1.609344))
+
---
+++ string_1.py
@@ -0,0 +1,14 @@
+ad="ayhan"
+soyad="kale"
+yas='37'
+msj='onun adı '+ad +' ve soyadı ' +soyad + ' yaşı ise ' + yas
+print(msj)
+print(len(msj)) # karekter sayısı
+print(msj[len(msj)-1])
+print(msj[-1])# sondan
+print(msj[5:10])# 5 karekterden basla 10. karaktere kadar al 
+
+print(msj[:10])# bastan basla 10. karaktere kadar al
+print(msj[5:]) # 5.karakter başla sona kadar git
+print(msj[5:40:3])# 5 karekterden basla 40. karaktere kadar 3 seferde bir karakter al
+print(msj[::-1])# tersten yazdırırır
