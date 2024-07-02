99321a46ee0d6371de8f537a9bfcf9fb08eee63b
sahan <101819553+omerfshan@users.noreply.github.com>
2024-07-03 02:12:28 +0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -9,6 +9,25 @@ ogrenci={100:{
     "notlar":[100,50,60]
     
 }}
-print(ogrenci[100]["ad"])
-print(ogrenci[100]["yas"])
-print(ogrenci[100]["notlar"][0])
+# print(ogrenci[100]["ad"])
+# print(ogrenci[100]["yas"])
+# print(ogrenci[100]["notlar"][0])
+sonuc=ogrenci.get(100)
+
+# for x in plakalar.values():
+#  print(x)
+
+for x,y in plakalar.items():
+ print(x,y)
+print("eskişekir"in plakalar )
+print(len(plakalar))
+plakalar.pop("izmir")
+del plakalar["aydın"]
+# del plakalar
+# plakalar.clear()
+plakalar2=plakalar #referans
+plakalar3=plakalar.copy()
+plakalar.update({
+ "kocaeli":23,
+ "balıkesir":10
+})
