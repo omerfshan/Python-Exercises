7e7aa21f6acaf8d2dd00d0bbfb385f508f8b885e
sahan <101819553+omerfshan@users.noreply.github.com>
2024-07-03 01:55:45 +0300

    add

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# key-value sıralanamaz
+plakalar= {'kocaeli':41,'istabul':34,'aydın':9}
+plakalar["rize"]=53
+plakalar["izmir"]=70
+plakalar["izmir"]=35 #70 to 35 
+ogrenci={100:{
+    "ad":"ali",
+    "yas":19,
+    "notlar":[100,50,60]
+    
+}}
+print(ogrenci[100]["ad"])
+print(ogrenci[100]["yas"])
+print(ogrenci[100]["notlar"][0])
---
+++ tuples.py
@@ -0,0 +1,6 @@
+# değiştirilemeyen liste ekleme silmede olmaz
+_list=[2,3,4]
+_tuple=(1,'a',"iki",True)
+# _tuple[0]=2
+newTuple=tuple(_list)
+print(newTuple+_tuple)
