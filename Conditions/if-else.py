ba5743ea3c5c7101d5f1bea04042af3b345def7b
sahan <101819553+omerfshan@users.noreply.github.com>
2024-07-03 12:40:49 +0300

    add

---
+++ Conditions/if-else.py
@@ -0,0 +1,18 @@
+username="admin"
+password=1234
+
+if (username=="admin") and (password==1234):
+    print("merhaba")
+else:
+    print("username veya parola yanlış")
+
+
+a=14
+
+if (a>0):
+    print("a pozitif")
+elif(a==0):
+   print("a pozitif veya negatif değil")
+else:
+  print("a negatif")
+
--- operators/comparison-demo.py
+++ operators/comparison-demo.py
@@ -3,3 +3,5 @@ y=int(input("y: "))
 
 sonuc=(x>y)
 sonuc=(x%2==0)
+sonuc=(x>0)
+sonuc=((x*0.4+y*0.6)>50)
