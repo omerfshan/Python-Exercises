5bf23acee4924f79103c082dbe80db295152f328
sahan <101819553+omerfshan@users.noreply.github.com>
2024-08-02 13:48:52 +0300

    add

--- exception/raise.py
+++ exception/raise.py
@@ -1,1 +1,21 @@
-def color
+def colorize(text,color):
+    colors=("blue","red","white","black","orange")
+    if type(text) is not str:
+        raise TypeError("text str tipinde olmalılıdır")
+    if type(color) is not str:
+        raise TypeError("renk str tipinde olmalılıdır")
+    if color not in colors:
+        raise ValueError("gecersiz renk girildi")
+    print(f"{text} {color} rengindedir")
+
+  
+try:
+  colorize("selam","yellow")
+except Exception as ex:
+  print(ex)
+
+try:
+   colorize("selam","yellow")
+except (TypeError,ValueError) as ex:
+   print(ex)
+   
